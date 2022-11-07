73de9ec560200cf27b02a7e0334e459e4f6d8bc0
Shapna <shapna088@gmail.com>
2022-11-08 04:40:36 +0600

    limit api added

--- index.js
+++ index.js
@@ -21,27 +21,35 @@ async function run() {
     try {
         const serviceCollection = client.db('TheMoments').collection('services');
 
-        //1.
-        app.get('/services', async(req, res) => {
+        // 1.
+        app.get('/home', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+
+        //2.
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
         })
 
-        //2.specific id'r jonno
+        //3.specific id'r jonno
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) }; 
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service)
-          })
+        })
 
     } finally {
-      
+
     }
-  }
-  run().catch(err=> console.error(err));
+}
+run().catch(err => console.error(err));
 
 
 //primary step
@@ -49,6 +57,6 @@ app.get('/', (req, res) => {
     res.send('server is running')
 });
 
-app.listen(port, (req, res) =>{
+app.listen(port, (req, res) => {
     console.log(`server is running on port ${port}`);
 })
