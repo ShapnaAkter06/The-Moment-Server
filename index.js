8ecdcfb658b9c70a6c3630680a10bdfac0975c67
Shapna <shapna088@gmail.com>
2022-11-08 00:31:12 +0600

    create api

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middle ware
 app.use(cors());
 app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qlhnchw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('TheMoments').collection('services');
+
+        //1.
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        //2.specific id'r jonno
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }; 
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+          })
+
+    } finally {
+      
+    }
+  }
+  run().catch(err=> console.error(err));
+
 
 //primary step
 app.get('/', (req, res) => {
